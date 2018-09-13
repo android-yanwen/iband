42606b8551c990187db55f83b15ab3d156871542
suting <su.ting@outlook.com>
2018-09-13 14:05:45 +0800

    1.网络判断 2.没有血压页面时，血氧页面标题显示错误的bug

--- app/src/main/java/com/manridy/iband/view/main/MainActivity.java
+++ app/src/main/java/com/manridy/iband/view/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.manridy.iband.view.main;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,6 +12,8 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -166,11 +169,59 @@ public class MainActivity extends BaseActivity {
                 case 2:
                     ivShare.setEnabled(true);
                     break;
+                case 3:
+                    showNoNetWorkDlg(MainActivity.this);
+                    break;
             }
         }
     };
 
-    //检测防丢免打扰时间段和开关
+
+    /*
+     * 判断网络连接是否已开
+     * true 已打开  false 未打开
+     * */
+    public static boolean isConn(Context context){
+        boolean bisConnFlag=false;
+        ConnectivityManager conManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo network = conManager.getActiveNetworkInfo();
+        if(network!=null){
+            bisConnFlag=conManager.getActiveNetworkInfo().isAvailable();
+        }
+        return bisConnFlag;
+    }
+
+
+
+    /**
+     * 当判断当前手机没有网络时选择是否打开网络设置
+     * @param context
+     */
+    public static void showNoNetWorkDlg(final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.mipmap.app_icon)         //
+                .setTitle(R.string.app_name)            //
+                .setMessage(R.string.hint_network_available).setPositiveButton(R.string.hint_set, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 跳转到系统的网络设置界面
+                Intent intent = null;
+                // 先判断当前系统版本
+                if (android.os.Build.VERSION.SDK_INT > 10) {  // 3.0以上
+//                    intent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                    intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+                } else {
+                    intent = new Intent();
+                    intent.setClassName("com.android.settings", "com.android.settings.WirelessSettings");
+                }
+                context.startActivity(intent);
+
+            }
+        }).setNegativeButton(R.string.hint_cancel, null).show();
+    }
+
+        //检测防丢免打扰时间段和开关
     private boolean checkLostDisturb() {
         String deviceName = (String) SPUtil.get(mContext, AppGlobal.DATA_DEVICE_BIND_NAME,"");
         String deviceVersion = (String) SPUtil.get(mContext, AppGlobal.DATA_FIRMWARE_VERSION,"1.0.0");
@@ -204,6 +255,10 @@ public class MainActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
 
+        if(!isConn(MainActivity.this)){
+            Message message = handler2.obtainMessage(3);
+            handler2.sendMessage(message);
+        }
 
 
         //判断语言版本
@@ -246,7 +301,7 @@ public class MainActivity extends BaseActivity {
                     }else{
                         isShowBo = true;
                     }
-                    SPUtil.put(mContext,"isShowBo",isShowBp);
+                    SPUtil.put(mContext,"isShowBo",isShowBo);
 //                    if ("0".equals("0")) {
 //                        isShowBp = false;
 //                    }
@@ -816,7 +871,11 @@ public class MainActivity extends BaseActivity {
                 tbTitle.setText(R.string.hint_view_hr);
                 break;
             case 3:
-                tbTitle.setText(R.string.hint_view_hp);
+                if(isShowBp){
+                    tbTitle.setText(R.string.hint_view_hp);
+                }else{
+                    tbTitle.setText(R.string.hint_view_bo);
+                }
                 break;
             case 4:
                 tbTitle.setText(R.string.hint_view_bo);
