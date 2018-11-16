cd439ebb72368a3d3992f2343ac3fc8863e53aa5
yw <314625433@qq.com>
2018-11-16 13:51:19 +0800

    常规提交

--- app/src/main/java/com/manridy/iband/ConfigurationParameter.java
+++ app/src/main/java/com/manridy/iband/ConfigurationParameter.java
@@ -3,12 +3,12 @@ package com.manridy.iband;
 public class ConfigurationParameter {
     //app的发布平台
     //google YinYongBao
-    public final static String publishPlatform = "google";
+    public final static String publishPlatform = "YinYongBao";
     //启动运动户外活动模式是否根据信号强度
     public final static boolean Is_startOutdoorsSport_accordingGpsSignal = true;
     //是否指定测试使用的地图
     public final static boolean Is_appoint_map = false;
-    //指定使用的地图
+    //指定使用的地图e
     //google GaoDe
     public final static String Appoint_map = "google";
 }
